f801c9db6b119132846f5405cdf3c0bac9bb5c73
Martin Kolárik <martin@kolarik.sk>
2022-04-15 12:11:38 +0200

    Add type annotations

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const STATIC_PATTERN = /^[=v]*\d+\.\d+\.\d+[0-9A-Za-z-]*$/;
 
+/**
+ * @param {string} version
+ * @returns {boolean}
+ */
 module.exports = (version) => {
 	return STATIC_PATTERN.test(version);
 };
